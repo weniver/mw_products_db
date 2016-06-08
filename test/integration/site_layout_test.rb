5405485fb3ee758d7da48c5becc5dfdf928b7405
Diego Cevallos Porta <weniver.github@gmail.com>
2016-06-08 14:17:05 -0500

    Adds Test for site layout

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", 'https://www.facebook.com/mamawolfmx/?fref=ts'
+  end
+
+
+end
