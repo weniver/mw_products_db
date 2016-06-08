ebfdc77e19cc3cf45f29fdc7a6d0b052dc6d548b
Diego Cevallos Porta <weniver.github@gmail.com>
2016-06-08 16:56:03 -0500

    Adds user model and signup

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  before_save { self.email = email.downcase }
+  validates :email, presence: true,
+                    length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :name,  presence: true,
+                    length: { maximum: 50 }
+  has_secure_password
+  validates :password, length: { minimum: 8 }
+end
