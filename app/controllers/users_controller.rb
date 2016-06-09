aeeca948e23ee616b1be2f477d28ad47f92d509f
Diego Cevallos Porta <weniver.github@gmail.com>
2016-06-09 17:42:41 -0500

    Adds signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Tu cuenta se creó exitosamente!'
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name,
+                                   :email,
+                                   :password,
+                                   :password_confirmation)
+    end
