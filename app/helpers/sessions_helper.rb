2e0747c3eed732d99b5a3d1f0de86b76148d9965
Diego Cevallos Porta <weniver.github@gmail.com>
2016-06-10 13:44:45 -0500

    Adds Login Page en session for logged user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+  #Logs in the User
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  #Returns Current User Logged-in
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  #Return TRUE if a user is logged_in
+  def logged_in?
+    !current_user.nil?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  root 'static_pages#home'
-  get 'signup'  => 'users#new'
+  root               'static_pages#home'
+  get    'signup' => 'users#new'
+  get    'login'  => 'sessions#new'
+  post   'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 end
