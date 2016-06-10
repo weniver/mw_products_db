c4cb48bc842c7fe5acaf5aff0b217acd31cc5a39
Diego Cevallos Porta <weniver.github@gmail.com>
2016-06-10 18:18:29 -0500

    Finish Logout and remember for persistent session

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   before_save { self.email = email.downcase }
   validates :email, presence: true,
@@ -15,5 +16,24 @@ class User < ActiveRecord::Base
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+  #Return Random Token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  #Remembers user in Database permanent cookie
+  def remember
+    self.remember_token = User.new_token
+    self.update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -28,6 +28,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
-    assert_select "a[href=?]", login_path
+    delete logout_path
+    follow_redirect!
+    assert_select "a[href=?]", login_path, count: 1
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_nil cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
   end
 end
