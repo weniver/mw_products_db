1c3044ad1d0e160a46ac6fd7d3e9ea2451e45666
Diego Cevallos Porta <weniver.github@gmail.com>
2016-06-14 00:18:53 -0500

    Adds reset password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   before_save :downcase_mail
   before_create :create_activation_digest
@@ -29,6 +29,11 @@ class User < ActiveRecord::Base
     self.update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   def forget
     update_attribute(:remember_digest, nil)
   end
@@ -49,7 +54,20 @@ class User < ActiveRecord::Base
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
-  
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+
     private
 
     def create_activation_digest
