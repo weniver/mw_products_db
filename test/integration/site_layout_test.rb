f365cb5c423d8f01942860f4af06f2d70648baa3
Diego Cevallos Porta <weniver.github@gmail.com>
2016-06-16 19:41:10 -0500

    Add Layout Test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,12 +1,26 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @admin = users(:diego)
+    @user = users(:ivonne)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", login_path, count: 2
     assert_select "a[href=?]", 'https://www.facebook.com/mamawolfmx/?fref=ts'
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", root_path, count: 1
+    assert_select "a[href=?]", users_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 1
+    assert_select "a[href=?]", logout_path, count: 1
+    assert_select "a[href=?]", edit_user_path(@user), count: 1
+    log_in_as(@admin)
+    get root_path
+    assert_select "a[href=?]", users_path, count: 1
+    assert_select "a[href=?]", signup_path, count: 1
   end
-
-
 end
