d9488666bc207b289a6ff28cff27706891a9c702
Diego Cevallos Porta <weniver.github@gmail.com>
2016-06-24 12:24:59 -0500

    Adds dataTables to Products
    Working with turbo links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,12 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
+//= require dataTables/jquery.dataTables
 //= require_tree .
+
+jQuery ->
+  $('#products').dataTable()
